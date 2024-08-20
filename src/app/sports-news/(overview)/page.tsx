4690deb35300f36540d3510be37bfb03976b84a9
Javier <japerez@nevada.unr.edu>
2024-08-20 01:33:59 -0700

    Suspense wrap on LatestNews as data fetching was added.

--- src/app/sports-news/(overview)/page.tsx
+++ src/app/sports-news/(overview)/page.tsx
@@ -2,6 +2,7 @@ import ContentFilter from "@/components/sports-news/content-filter";
 import LatestNews from "@/components/sports-news/latest-news";
 
 import Divider from '@mui/material/Divider';
+import { Suspense } from "react";
 
 export default async function Page({
     searchParams,
@@ -17,10 +18,26 @@ export default async function Page({
     return (
         <>
             {/* Main Body */}
-            {/* <NewsFilter /> */}
             <ContentFilter sport={sport}/>
             <Divider sx={{ my: 2 }} />
-            <LatestNews sport={sport} league={league}/>
+            <Suspense fallback={<div>Loading Latest News...</div>}>
+                <LatestNews sport={sport} league={league}/>
+            </Suspense>
         </>
     );
 }
+
+
+/*
+    Note:
+    Issue found with the way content filter and params are being passed to the page component.
+    The searchParams are simply retrieved by this page and passed to the content filter and
+    latest news, however there are no checks to see if the searchParams are valid or if they
+    correspond to any actual sports or leagues. This could lead to errors if the searchParams
+    are updated manually by the user in the browser.
+
+    In content filter, the actual sport options and corresponding league options for those sports
+    are retrieved. Depending on how I handle errors, it may be best to check the validity of the
+    sport and league in the page component before passing them to the child components.
+
+*/
