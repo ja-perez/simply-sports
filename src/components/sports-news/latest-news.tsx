8e105b44213066cf4047b80f9e182b63d9544a08
Javier <japerez@nevada.unr.edu>
2024-08-19 21:57:05 -0700

    Added Article-card component

--- src/components/sports-news/latest-news.tsx
+++ src/components/sports-news/latest-news.tsx
@@ -15,3 +15,52 @@ export default async function LatestNews({
         </>
     );
 }
+
+interface articleRibbonProps {
+    source: string;
+    articles: string[];
+}
+
+function articleRibbon({
+    source,
+    articles,
+}: articleRibbonProps) {
+    return (
+        <>
+        </>
+    );
+}
+
+import { Article } from '@/lib/definitions';
+import Card from '@mui/material/Card';
+import { CardActionArea } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+function articleCard({ article }: { article: Article }) {
+    return (
+        <>
+        <Card>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height={article.image.height}
+                    width={article.image.width}
+                    image={article.image.href}
+                    alt={article.image.alt}
+                />
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        {article.title}
+                    </Typography>
+                    <Typography variant="body2" >
+                        {article.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+        </>
+    );
+}
