fa860135577c3f111027d10aed69df5762d40683
Javier <japerez@nevada.unr.edu>
2024-08-23 07:32:29 -0700

    Updating db script to match refactoring of API classes

--- backend/db.py
+++ backend/db.py
@@ -1,19 +1,22 @@
 from pymongo import MongoClient, TEXT
 from dotenv import dotenv_values
-
-import datetime
 import os
 
 from news_articles.guardian.api import GuardianAPI
 from news_articles.espn.api import ESPNAPI
 
 
-def insert_articles(collection, articles: list):
-    for article in articles:
-        collection.insert_one(article.to_dict())
+def process_api_data(api) -> None:
+    content = api.fetch_sports_content()
+    processed_content = api.process_sports_content(content)
+    if not processed_content:
+        print("No new articles to upload")
+        return
+    print(f"Uploading {api.__class__.__name__} articles to MongoDB")
+    upload_articles_to_mongo(processed_content)
 
 
-def main():
+def upload_articles_to_mongo(processed_content) -> None:
     config = dotenv_values(".env")
     env_mode = config["MODE"]
     db_name = config["MONGO_NAME"]
@@ -28,52 +31,41 @@ def main():
     db = client[db_name]
     collection = db["articles"]
 
-    # Guardian API
+    for sport in processed_content:
+        leagues = processed_content[sport]
+        for league in leagues:
+            articles = leagues[league]
+            insert_count = 0
+            for article in articles:
+                if collection.find_one({"article_id": article["article_id"]}):
+                    continue
+                collection.insert_one(article)
+                insert_count += 1
+            print(f"Inserted {insert_count} articles for {sport} - {league}")
+    
+
+def main():
+    # Base file paths
     cwd = os.getcwd()
     if "backend" not in cwd:
         cwd = os.path.join(cwd, "backend", "news_articles")
     elif "news_articles" not in cwd:
         cwd = os.path.join(cwd, "news_articles")
+
+    # Guardian API
     guardian_folder_path = os.path.join(cwd, "guardian")
     guardian_api_data_path = os.path.join(guardian_folder_path, "api.data.json")
-    current_date = datetime.datetime.now()
-    start_of_year = datetime.datetime(current_date.year, 1, 1)
     guardian = GuardianAPI(
-        previous_fetch_date=start_of_year,
+        api_key="test",
         api_data_path=guardian_api_data_path)
-    sports = guardian.collect_api_data()
-
-    for sport in sports:
-        leagues = sports[sport] 
-        for league in leagues:
-            articles = leagues[league]
-            for article_data in articles:
-                images = [1 if elem["type"] == "image" else 0 for elem in article_data["elements"]]
-                if 1 not in images:
-                    continue
-                article = guardian.format_article(
-                    sport, 
-                    league,
-                    article_data)
-                if collection.find_one({"article_id": article["article_id"]}):
-                    continue
-                collection.insert_one(article)
+    process_api_data(guardian)
 
     # ESPN API
     espn_folder_path = os.path.join(cwd, "espn")
     espn_api_data_path = os.path.join(espn_folder_path, "api.data.json")
     espn = ESPNAPI(espn_api_data_path)
+    process_api_data(espn)
 
-    api_data = espn.collect_api_data()
-    articles = espn.process_articles(api_data)
-    output_file_path = os.path.join(espn_folder_path, "articles.json")
-    espn.write_data_to_file(output_file_path, articles)
-    # upload articles
-    for index in articles:
-        article = articles[index]
-        if collection.find_one({"article_id": article["article_id"]}):
-            continue
-        collection.insert_one(article)
 
 if __name__ == "__main__":
     main()
