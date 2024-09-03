2d9de2c526e236824c13612574c5ffc451e4fc3b
Javier <japerez@nevada.unr.edu>
2024-09-03 13:13:09 -0700

    Installed mdx packages for writing learn page content

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm';
+import rehypePrism  from 'rehype-prism-plus';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import rehypeCodeTitles from 'rehype-code-titles';
+import { visit } from "unist-util-visit";
+
 /** @type {import('next').NextConfig} */
 
 const imageConfig =
@@ -6,8 +14,45 @@ const imageConfig =
     : {}
 
 const nextConfig = {
+    // Configure 'pageExtensions' to include markdown and MDX files
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     assetPrefix: process.env.MODE === 'prod' ? '/team19/' : '',
     ...imageConfig,
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [
+            preProcess,
+            rehypePrism,
+            rehypeAutolinkHeadings,
+            rehypeSlug,
+            rehypeCodeTitles,
+            postProcess
+        ]
+    }
+});
+
+function preProcess(tree) {
+    visit(tree, (node) => {
+        if (node?.type === "element" && node?.tagName === "pre") {
+            const [codeEl] = node.children;
+            if (codeEl.tagName !== "code") return;
+            node.raw = codeEl.children?.[0].value;
+        }
+    });
+};
+
+function postProcess(tree) {
+    console.log(1)
+    visit(tree, "element", (node) => {
+        if (node?.type === "element" && node?.tagName === "pre") {
+            node.properties["raw"] = node.raw;
+        }
+    });
+};
+
+
+export default withMDX(nextConfig);
