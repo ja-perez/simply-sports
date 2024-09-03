78d51d74754477f5b319d9ffcb3782947c7bfaa1
Javier <japerez@nevada.unr.edu>
2024-09-03 13:17:08 -0700

    Created new file in lib to define routes and navigation links, to replace nav-links in components

---
+++ src/lib/routes-config.ts
@@ -0,0 +1,48 @@
+export type NavLink = {
+    id: string;
+    label: string;
+    href: string;
+    sections?: Array<NavLink>;
+}
+
+export const topBarRoutes: Array<NavLink> = [
+    {
+        id: "home",
+        label: "Home",
+        href: "/",
+        sections: [
+            { id: "about", label: "About", href:"/about"},
+            { id: "features", label: "Features", href:"/features"},
+            { id: "background", label: "background", href:"/background"},
+        ]
+    },
+    {
+        id: "news-teams",
+        label: "News & Teams",
+        href: "news-teams",
+    },
+    {
+        id: "learn",
+        label: "Learning Center",
+        href: "learn",
+        sections: [
+            { id: "introduction", label: "Introduction", href:"introduction",
+                sections: [
+                    { id: "bet-types", label: "Bet Types", href:"bet-types"}
+                ]
+            },
+            { id: "terminology", label: "Terminology", href:"terminology"},
+            { id: "resources", label: "Resources", href:"resources"},
+        ]
+    },
+    {
+        id: "practice",
+        label: "Practice Lounge",
+        href: "practice",
+        sections: [
+            { id: "menu", label: "Menu", href: "menu" },
+            { id: "dashboard", label: "Dashboard", href: "dashboard" },
+            { id: "tutorial", label: "Tutorial", href: "tutorial" },
+        ]
+    }
+]
