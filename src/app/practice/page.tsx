3478ee168bd67cfc631d843724b2e65afa2c7e39
Javier <japerez@nevada.unr.edu>
2024-09-08 16:59:07 -0700

    Moved contents of menu component into practice page file and created new component file for menu actions

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -1,9 +1,46 @@
-import Menu from "@/components/practice/menu"
+import Grid from '@mui/material/Grid';
+
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
+import Disclaimer from '@/components/practice/disclaimer';
+import MenuActions from '@/components/practice/menu-actions';
+import CustomLink from '@/components/custom-link';
 
 export default function Practice() {
     return (
         <>
-        <Menu />
+        <Grid container spacing={2}
+            sx={{
+                display: 'flex',
+            }}
+        >
+            <Grid item xs={12}>
+                <Typography variant="h2">
+                    Practice Lounge
+                </Typography>
+                </Grid>
+
+            <Grid item xs={12}>
+                <Typography variant="subtitle1">
+                    Put your gambling skills to the test with our simulated betting sessions. Choose from over 100+
+                    matches spanning various sports, leagues, and seasons. If you find yourself losing one to many
+                    fictional dollars, visit the&nbsp;
+                    <CustomLink href="learn" {...{className:"underline"}}>Learning Academy</CustomLink>
+                    &nbsp;for some tips and breakdowns on what goes into a &quot;good&quot; bet.
+                    </Typography>
+                </Grid>
+
+            <Grid item xs={12}>
+                <Divider aria-hidden="true"/>
+                </Grid>
+
+            <Grid item xs={12}>
+                <Disclaimer />
+                </Grid>
+
+            <MenuActions />
+        </Grid>
         </>
     );
 }
---
+++ src/components/practice/menu-actions.tsx
@@ -0,0 +1,177 @@
+'use client';
+import { useState } from 'react';
+
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+import Collapse from '@mui/material/Collapse';
+
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
+import { Match } from '@/lib/definitions';
+
+const columns: GridColDef[] = [
+    { field: 'label', headerName: 'Match Name', width:550},
+    { field: 'date', headerName: 'Date', width:250},
+]
+
+export default function MenuActions() {
+    const [showTable, setShowTable]  = useState(false);
+    const [match, setMatch] = useState<Match | null>(null);
+    const [matches, setMatches] = useState<Array<Match>>([]);
+
+    function resetStates() {
+        setMatch(null);
+        setMatches([]);
+        setShowTable(false);
+    }
+
+    async function handleAllMatches() {
+        if (showTable) {
+            resetStates()
+        } else {
+            setMatch(null);
+            setShowTable(!showTable);
+            (await fetch('/api/matches')).json()
+            .then((data: Array<Match>) => {
+                setMatches(data)
+            })
+        }
+    }
+
+    async function handleRandomMatch() {
+        if (showTable) resetStates();
+        (await fetch('/api/matches/random')).json()
+        .then((data: Match) => {
+            if (typeof data.date === 'string') {
+                const matchDate = new Date(data.date)
+                data.date = matchDate.toDateString();
+            }
+            setMatch(data)
+        });
+    }
+
+    return (
+        <>
+        <Grid item xs={12}>
+            <Card sx={{ display:'flex', flexDirection:'column'}}>
+                <CardHeader
+                    title="Start a practice betting session"
+                    />
+
+                <Divider flexItem variant="middle"/>
+
+                <CardContent sx={{ display:'flex', justifyContent:'space-around'}}>
+                    <Button  sx={{marginY:"5px"}} onClick={handleAllMatches}
+                        variant={showTable ? "contained" : "outlined"}
+                        >
+                        All matches
+                        </Button>
+                    <Button variant="outlined" sx={{marginY:"5px"}} onClick={handleRandomMatch}>
+                        Random match
+                        </Button>
+
+                    {process.env.MODE === "prod"
+                    ? null
+                    : <Button id="dev" href="practice/session" variant="contained">
+                        practice session
+                        </Button>
+                    }
+                </CardContent>
+            </Card>
+        </Grid>
+
+        <Grid item xs={12}>
+            <Collapse in={showTable}>
+                <DataGrid
+                    loading={!matches.length}
+                    slotProps={{
+                        loadingOverlay: {
+                            variant: 'skeleton',
+                            noRowsVariant: 'skeleton'
+                        }
+                    }}
+                    sx={{ backgroundColor: 'white' }}
+                    rows={matches}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 5,
+                            },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                    onRowSelectionModelChange={(ids) => {
+                        const selectedIds = new Set(ids);
+                        const selectedRowData = matches.filter((match) =>
+                            selectedIds.has(match.id.toString()))
+                        const selectedMatch = selectedRowData[0];
+                        setMatch(selectedMatch);
+                    }}
+                    />
+            </Collapse>
+        </Grid>
+
+        <Grid item xs={12}>
+            <MatchSummary match={match} />
+        </Grid>
+        </>
+    )
+}
+
+
+function MatchSummary({
+    match
+}: { match: Match | null }) {
+    if (match === undefined) match = null;
+    const matchHeader = match !== null
+    ? (match.metadata.sport === 'soccer'
+        ? `${match.metadata.sport} - ${match.metadata.season}`.toUpperCase()
+        : `${match.metadata.sport} - ${match.metadata.league} - ${match.metadata.seasonId} ${match.metadata.season}`.toUpperCase()
+    ) : 'No match selected'
+
+    return (
+        <>
+        <Card>
+            <CardHeader
+                title={matchHeader}
+                titleTypographyProps={{ variant:'subtitle1' }}
+                />
+            <Divider flexItem variant="middle" />
+            <CardContent
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+                >
+                <div className="w-3/4 px-2 flex justify-center min-h-20"
+                >
+                    <Container className="rounded-md bg-[#0000001f]">
+                        <Typography variant="h4">
+                            {match !== null ? `${match.label}` : '-'}
+                            </Typography>
+                        <Typography variant="subtitle1">
+                            {match !== null ? `${match.date} @ ${match.venue.name}` : '-'}
+                            </Typography>
+                    </Container>
+                    </div>
+                <div className="w-1/4 px-2 flex justify-center"
+                >
+                        <Button size="large" disabled={match === null} variant="contained">
+                            Start Practice
+                        </Button>
+                </div>
+            </CardContent>
+        </Card>
+        </>
+    )
+}
+
+
